446724b2119ab0b843ae81880dcc1d92ad49a672
muthomi254 <dmunene202020@gmail.com>
2023-09-29 02:21:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// // Write your code here!
+
+// newHeader = document.querySelector('h1#victory');
+
+// const mainElement = document.querySelector('main#main');
+
+const main = document.querySelector('main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+
+document.body.appendChild(newHeader);
+
